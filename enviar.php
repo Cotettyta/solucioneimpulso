ed31627ad2a500a8a8c61b38aaf5dcdd0492fba0
maryjosuazo@gmail.com <cote241285>
2017-03-29 23:53:22 -0300

    subiendo modificaciones

--- enviar.php
+++ enviar.php
@@ -2,9 +2,9 @@
 header("Content-type: text/html;charset=\"utf-8\"");
 $nombre = $_POST['nombre'];
 $mail = $_POST['email'];
-$telefono = $_POST['telefono'];
+$empresa = $_POST['empresa'];
 $asunto = $_POST['asunto'];
-$empresa = $_POST['mensaje'];
+$mensaje = $_POST['mensaje'];
 
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
@@ -13,16 +13,16 @@ $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Empresa: " . $empresa . " \r\n";
 $mensaje .= "Asunto: " . $asunto . " \r\n";
-$mensaje .= "Teléfono: " . $telefono . " \r\n";
-$mensaje .= "Mensaje: " . $empresa . " \r\n";
+$mensaje .= "Mensaje: " . $mensaje . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'maryjosuazo@gmail.com';
+$para = 'contacto@solucioneimpulso.cl';
 $asunto = 'Mensaje de mi sitio web';
 
 if (mail($para, $asunto, utf8_decode($mensaje), $header))
 echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado exitosamente');</script>";
-echo "<script type='text/javascript'>window.location.href='https://cotettyta.github.io/solucioneimpulso1//';</script>";
+echo "<script type='text/javascript'>window.location.href='https://www.solucioneimpulso.cl';</script>";
 
 ?>
